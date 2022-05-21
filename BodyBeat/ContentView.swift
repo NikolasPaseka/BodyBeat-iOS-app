38888a2b4a40332b25a8b2ba4e4b4411e5dd3463
Nikolas Paseka <nikolas.paseka7@gmail.com>
2022-05-22 00:02:56 +0200

    bottom navigation
    added bottom navigation. Reorganized project structure

--- BodyBeat/ContentView.swift
+++ BodyBeat/ContentView.swift
@@ -9,86 +9,39 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Plan.title, ascending: true)],
-        animation: .default)
-    private var plans: FetchedResults<Plan>
-    
-    @State var isNewPlanVisible: Bool = false
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(plans) { plan in
-                    NavigationLink(destination: PlanDetailView(plan: plan)) {
-                        Text("\(plan.title!) \(plan.exercises?.count ?? 0)")
-                    }
+        TabView {
+            PlanListView()
+                .tabItem {
+                    Image(systemName: "list.dash")
+                    Text("Plans")
                 }
-                .onDelete(perform: deleteItems)
-                .listRowBackground(Color("lighterGrey"))
-            }.background(Color("backgroundGrey"))
-            .navigationTitle("Workout plans")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+            
+            Text("Schedule")
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("Schedule")
                 }
-                ToolbarItem {
-                    NavigationLink(destination: NewPlanView(isNewPlanVisible: $isNewPlanVisible),
-                                   isActive: $isNewPlanVisible) {
-                        Button(action: {
-                            isNewPlanVisible = true
-                        }) {
-                            Label("Add Item", systemImage: "plus")
-                        }
-                    }
+            
+            Text("Parks")
+                .tabItem {
+                    Image(systemName: "location")
+                    Text("Parks")
                 }
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newPlan = Plan(context: viewContext)
-            newPlan.title = "Test"
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { plans[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
---
+++ BodyBeat/View/PlanListView.swift
@@ -0,0 +1,94 @@
+//
+//  PlanListView.swift
+//  BodyBeat
+//
+//  Created by Nikolas Paseka on 21.05.2022.
+//
+
+import SwiftUI
+import CoreData
+
+struct PlanListView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Plan.title, ascending: true)],
+        animation: .default)
+    private var plans: FetchedResults<Plan>
+    
+    @State var isNewPlanVisible: Bool = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(plans) { plan in
+                    NavigationLink(destination: PlanDetailView(plan: plan)) {
+                        Text("\(plan.title!) \(plan.exercises?.count ?? 0)")
+                    }
+                }
+                .onDelete(perform: deleteItems)
+                .listRowBackground(Color("lighterGrey"))
+            }.background(Color("backgroundGrey"))
+            .navigationTitle("Workout plans")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+                ToolbarItem {
+                    NavigationLink(destination: NewPlanView(isNewPlanVisible: $isNewPlanVisible),
+                                   isActive: $isNewPlanVisible) {
+                        Button(action: {
+                            isNewPlanVisible = true
+                        }) {
+                            Label("Add Item", systemImage: "plus")
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+            let newPlan = Plan(context: viewContext)
+            newPlan.title = "Test"
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { plans[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+struct PlanListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
